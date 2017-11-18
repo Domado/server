c8c6fa361ed99bc4ee8d12ae903e72e9da9b4b23
Domado <2641324669@qq.com>
2017-11-18 17:58:38 +0800

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,8 @@
+// array of Monitor-specific API keys or Main API key to list all monitors
+var __apiKeys = [
+	'u516315-042a4671270748af6843a185', //API
+
+];
+
+// refresh interval (in seconds)
+var __refresh = 180;
